5aa02d1de8baa7635df3d3bbe4acd9195094af84
Enric Gimenez <iessimarrocicles@gmail.com>
2024-12-08 09:47:40 +0100

    Segon commit

--- src/main/java/org/simarro/Demo1Application.java
+++ src/main/java/org/simarro/Demo1Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Demo1Application {
 
+    // Esto es una prueba
     public static void main(String[] args) {
         SpringApplication.run(Demo1Application.class, args);
     }
