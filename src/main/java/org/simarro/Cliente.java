4e5b4e9066ea8e106ef353d7f4a32bafd9d24590
Enric Gimenez <iessimarrocicles@gmail.com>
2024-12-08 10:42:11 +0100

    Segon commit

---
+++ src/main/java/org/simarro/Cliente.java
@@ -0,0 +1,10 @@
+package org.simarro;
+
+import lombok.Data;
+
+@Data
+public class Cliente {
+    private String name;
+    private String email;
+    private String phone;
+}
---
+++ src/main/java/org/simarro/Products.java
@@ -0,0 +1,10 @@
+package org.simarro;
+
+import lombok.Data;
+
+@Data
+public class Products {
+
+    private Long id;
+    private String productName;
+}
